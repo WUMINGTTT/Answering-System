efaeebd2f1da583db29e6c79207bc09c7fe43290
WUMING_TT <3119852007@qq.com>
2026-05-22 10:05:36 +0800

    前端：添加登录页骨架

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
 import DisplayView from '@/views/DisplayView.vue'
 import PlayerView from '@/views/PlayerView.vue'
 import AdminView from '@/views/AdminView.vue'
@@ -11,6 +12,11 @@ const router = createRouter({
       path: '/',
       name: '首页',
       component: HomeView,
+    },
+    {
+      path: '/login',
+      name: '登录页',
+      component: LoginView,
     },
     {
       path: '/display',
